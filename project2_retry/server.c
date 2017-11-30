30ce3391790a6402eb966e3a01b03f625b908919
lbchavarria <lbchavarria@gmail.com>
2017-11-30 14:35:55 -0800

    update for pre-testing

--- project2_retry/server.c
+++ project2_retry/server.c
@@ -263,34 +263,34 @@ void client_data_handler() {
     printf("1\n");
     int nread;
     printf("Pre-Start\n");
-    CData *cd = (CData *)malloc(sizeof(CData));
+    CData cd;
     unsigned int len = (unsigned int)sizeof(struct sockaddr_in);
     printf("Start\n");
     nread = recvfrom(sockid, &cd, sizeof(CData), 0, (struct sockaddr *)&client_addr, &len);
     if (nread > 0) {
         printf("Receive successful\n");
-        if (cd->type == LOGIN) {
+        if (cd.type == LOGIN) {
             printf("Login\n");
-            user_login((CData *)&cd);
+            user_login(&cd);
         }
-        else if (cd->type == LOGOUT) {
+        else if (cd.type == LOGOUT) {
             user_logout();
         }
-        else if (cd->type == JOIN) {
-            user_join((CData *)&cd);
+        else if (cd.type == JOIN) {
+            user_join(&cd);
         }
-        else if (cd->type == LEAVE) {
-            user_leave((CData *)&cd);
+        else if (cd.type == LEAVE) {
+            user_leave(&cd);
         }
-        else if (cd->type == SAY) {
-            user_say((CData *)&cd);
+        else if (cd.type == SAY) {
+            user_say(&cd);
         }
     }
     else {
         printf("Failed to receive data from client");
     }
-    printf("Pre-Free\n");
-    free(cd);
+    //printf("Pre-Free\n");
+    //free(cd);
     printf("FREE\n");
 }
 
